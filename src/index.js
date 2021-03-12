f6d1ea8b5b6f8ce6942ac329af4e892a28659351
dima- <dima-tkachenko@mail.ru>
2021-03-12 22:28:28 +0300

    Модули и оптимизация сборки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World')
+import AppService from './modules/app.service'
+import {config} from './modules/config'
+import './modules/header.component'
+
+console.log('Config key:', config.key)
+
+const service = new AppService('Hello world!')
+service.log()
