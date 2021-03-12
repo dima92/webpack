0b8edeae9a709f44f747bf292b709dc0fed6062d
dima- <dima-tkachenko@mail.ru>
2021-03-13 00:14:21 +0300

    Работа с библиотеками

--- src/modules/header.component.js
+++ src/modules/header.component.js
@@ -1,1 +1,9 @@
-console.log('Header component')
+import $ from 'jquery'
+
+$('<h1 />')
+    .text('Hello world from JQuery')
+    .css({
+        textAlign: 'center',
+        color: 'blue'
+    })
+    .appendTo($('header'))
