46b6503f5e56ae244ae63125bae5018873665278
dima- <dima-tkachenko@mail.ru>
2021-03-12 23:58:34 +0300

    Плагины и создание HTML - файла

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HTMLPlugin({
+            filename: "index.html",
+            template: "./src/index.html"
+        })
+    ]
 }
