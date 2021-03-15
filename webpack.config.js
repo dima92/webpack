772ccf1093b65615e6b1389d3b815332b25669c0
dima- <dima-tkachenko@mail.ru>
2021-03-15 03:32:49 +0300

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
             })
         ]
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        compress: true,
+        port: 4200
+    },
     plugins: [
         new HTMLPlugin({
             filename: "index.html",
